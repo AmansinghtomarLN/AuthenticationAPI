fba5ad0f610d405cf160a7f9b40cec9fee118d42
Ruchi <ruchi27tiwari@gmail.com>
2026-01-23 20:09:26 +0530

    Updated files

--- src/main/java/com/api/auth/controller/MyController.java
+++ src/main/java/com/api/auth/controller/MyController.java
@@ -1,43 +1,22 @@
 package com.api.auth.controller;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.api.auth.dto.Userinfo;
+import com.api.auth.repository.DataBaseConnection;
+
 @RestController
 public class MyController {
 
+	DataBaseConnection db=new DataBaseConnection();
     @PostMapping("/signup")
-    public Data_1 signup(@RequestBody Data_1 user) throws ClassNotFoundException, SQLException {
-
-            Class.forName("com.mysql.cj.jdbc.Driver");
-
-            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/authenticationapidb","root","root");
-
-            String sql = "INSERT INTO userInfo (EmailId, Password, FirstName, LastName, Gender, DOB, Age, Mobno, Country)VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
-
-            PreparedStatement ps = con.prepareStatement(sql);
-
-            ps.setString(1, user.getEmail());
-            ps.setString(2, user.getPassword());
-            ps.setString(3, user.getFirstName());
-            ps.setString(4, user.getLastName());
-            ps.setString(5, user.getGender());
-            ps.setString(6, user.getDob());
-            ps.setInt(7, user.getAge());
-            ps.setLong(8, user.getMobileno());
-            ps.setString(9, user.getCountry());
-
-            ps.executeUpdate();
+    public Userinfo signup(@RequestBody Userinfo user) throws ClassNotFoundException, SQLException {
           
+    	db.saveUser(user);
            return user;
-           
-
-        
+          
     }
 }
