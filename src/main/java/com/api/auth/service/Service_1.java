ad32564c65c391b9054f4386a72dd307ee916387
Ruchi <ruchi27tiwari@gmail.com>
2026-01-24 14:09:59 +0530

    service class modified

--- src/main/java/com/api/auth/service/Service_1.java
+++ src/main/java/com/api/auth/service/Service_1.java
@@ -5,6 +5,7 @@ public class Service_1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Service_1 class");
+		System.out.println("Serviceeeee");
 
 	}
 
